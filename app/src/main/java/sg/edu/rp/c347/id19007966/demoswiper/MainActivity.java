c00e76cd1358f41e748bb2dcead36427a4569233
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-06-01 11:51:17 +0800

    up to Section B

--- app/src/main/java/sg/edu/rp/c347/id19007966/demoswiper/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19007966/demoswiper/MainActivity.java
@@ -1,14 +1,37 @@
 package sg.edu.rp.c347.id19007966.demoswiper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Fragment> al;
+    MyFragmentPagerAdapter adapter;
+    ViewPager vPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vPager = findViewById(R.id.viewPager);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        al = new ArrayList<Fragment>();
+        al.add(new Frag1());
+        al.add(new Frag2());
+
+        adapter = new MyFragmentPagerAdapter(fm, al);
+
+        vPager.setAdapter(adapter);
+
+
     }
 }
